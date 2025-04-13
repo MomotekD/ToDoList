f6d9264e88dca7d479c92b4c338bdc2ca06787fb
MomotekD <86006101+MomotekD@users.noreply.github.com>
2025-04-13 16:41:46 +0200

    Add: implement modal functionality and update HTML title

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import './styles.css';
+import './Components/modal.js';
+import { setupModals } from './Components/modal.js';
+
+document.addEventListener('DOMContentLoaded', () => {
+    setupModals();
+})
+
