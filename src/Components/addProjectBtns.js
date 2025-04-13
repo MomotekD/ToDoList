66cd55ba4f166958826089de6e45478bba110046
MomotekD <86006101+MomotekD@users.noreply.github.com>
2025-04-14 00:46:17 +0200

    Add: display todos in the toDoContainer on project button click

--- src/Components/addProjectBtns.js
+++ src/Components/addProjectBtns.js
@@ -7,6 +7,16 @@ export function addProjectBtns(){
     projects.forEach((project) => {
         const projectBtn = document.createElement('button');
         projectBtn.textContent = `${project.name}`;    
-        projectsBtns.appendChild(projectBtn);    
+        projectsBtns.appendChild(projectBtn);
+        projectBtn.addEventListener('click', (event) => {
+            const toDoContainer = document.querySelector('.toDoContainer')
+            toDoContainer.innerHTML = '';
+            
+            project.todos.forEach((todo) => {
+                const toDoTitle = document.createElement('h4');
+                toDoTitle.textContent = `${todo.name}`;
+                toDoContainer.appendChild(toDoTitle);
+            })
+        });
     })
 }
